56ad94d2525487134492100939e8f78e113d7b1f
jungai <juworaphol125@gmail.com>
2022-01-26 18:57:43 +0700

    add example

--- scripts/src/docker_deploy_on_vps.ts
+++ scripts/src/docker_deploy_on_vps.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env zx
 import { $ } from "zx";
+import { getSomeKey } from "../utils/get_env";
 
 void (async () => {
+  console.log(getSomeKey());
   console.log("deploy 🚀");
 })();
---
+++ scripts/utils/get_env/index.ts
@@ -0,0 +1,11 @@
+function getEnv(key: string): string {
+  const val = process.env[key];
+
+  if (!val) throw new Error(`${key} is not defined`);
+
+  return val;
+}
+
+export function getSomeKey() {
+  return getEnv("SOME_KEY");
+}
